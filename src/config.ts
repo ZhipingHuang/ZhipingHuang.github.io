2122a3b768308c299ebc43950eb2d6c631da1ef8
zphuang <zphuang1224@163.com>
2025-08-25 11:24:03 +0800

    修改github actions自动部署配置

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
-	subtitle: "One demo website",
+	subtitle: "生活明朗,万物可爱~_~",
 
 	lang: SITE_LANG,
 
